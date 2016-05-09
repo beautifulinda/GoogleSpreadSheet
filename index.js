564bb5313ae802bf8269f65431049559bb578db5
Linda Yeh <butylinda@icsc.com.tw>
2016-05-09 17:07:54 +0800

    add CORS on expressJs

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 // http://localhost:3000/googlesheet/addRow 
 app.post('/googlesheet/addRow', function(req, res) {
